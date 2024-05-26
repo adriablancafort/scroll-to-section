45e71108077d2776dceb7988db0a95bd64eb5d9b
Adria Blancafort <adriablancafort@gmail.com>
2024-05-26 21:58:25 +0200

    added click element functionality

--- content-script.js
+++ content-script.js
@@ -6,8 +6,18 @@ const scrollToElement = (classNames) => {
   }
 };
 
+const clickElement = (classNames) => {
+  const selector = classNames.split(" ").join(".");
+  const element = document.querySelector(`.${selector}`);
+  if (element) {
+    element.click();
+  }
+};
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "scrollToElement") {
     scrollToElement(message.classNames);
+  } else if (message.action === "clickElement") {
+    clickElement(message.classNames);
   }
 });
--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   const scrollToElementInput = document.getElementById("scrollToElementInput");
   const scrollToElementButton = document.getElementById("scrollToElementButton");
+  
+  const clickElementInput = document.getElementById("clickElementInput");
+  const clickElementButton = document.getElementById("clickElementButton");
 
   scrollToElementButton.addEventListener("click", () => {
     const classNames = scrollToElementInput.value;
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(tabs[0].id, {
         action: "scrollToElement",
+        classNames: classNames,
+      });
+    });
+  });
+
+  clickElementButton.addEventListener("click", () => {
+    const classNames = clickElementInput.value;
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "clickElement",
         classNames: classNames,
       });
     });
