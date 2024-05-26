2a070c3789e2b89bc81c74bdf0246f7138fb5f3d
Adria Blancafort <adriablancafort@gmail.com>
2024-05-26 22:05:19 +0200

    separate function to return an element given a list of classes

--- content-script.js
+++ content-script.js
@@ -1,23 +1,26 @@
-const scrollToElement = (classNames) => {
+const getElement = (classNames) => {
   const selector = classNames.split(" ").join(".");
   const element = document.querySelector(`.${selector}`);
+  return element;
+};
+
+const scrollToElement = (element) => {
   if (element) {
     element.scrollIntoView({ behavior: "smooth" });
   }
 };
 
-const clickElement = (classNames) => {
-  const selector = classNames.split(" ").join(".");
-  const element = document.querySelector(`.${selector}`);
+const clickElement = (element) => {
   if (element) {
     element.click();
   }
 };
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  const element = getElement(message.classNames);
   if (message.action === "scrollToElement") {
-    scrollToElement(message.classNames);
+    scrollToElement(element);
   } else if (message.action === "clickElement") {
-    clickElement(message.classNames);
+    clickElement(element);
   }
 });
